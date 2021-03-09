326bd6a29b092469bbb13a274d75db9d5233260c
FilipeLageGarcia <filipelagegarcia@gmail.com>
2021-03-09 01:35:43 -0300

    concluido

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+            string n = Console.ReadLine();
+            string v = "";
+            
+            int tamanho = n.Length;
+            for(int i = tamanho-1; i >= 0; i--)
+            {
+            v = String.Concat(v,n[i]);
+            }
+            Console.WriteLine(v);
+
+    }
+
+}
